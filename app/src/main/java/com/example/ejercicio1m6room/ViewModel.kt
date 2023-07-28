687331c2d05eb4495547b757dad83b1feac88744
Ana <almendras451@gmail.com>
2023-07-27 22:25:21 -0400

    creamos el viewModel pero no esta implementado

---
+++ app/src/main/java/com/example/ejercicio1m6room/ViewModel.kt
@@ -0,0 +1,23 @@
+package com.example.ejercicio1m6room
+
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.launch
+
+class ViewModel(aplicacion : Application) : AndroidViewModel(aplicacion){
+    private val repositorio:RepositorioTarea
+    init{
+        repositorio = RepositorioTarea(BaseDatosTarea.getDatabase(aplicacion).getDaoTarea())
+    }
+
+    fun obtenerTarea(): LiveData<List<TareaC>>{
+        return repositorio.listarTarea()
+    }
+    fun insertarTareas(tareaC: TareaC)=viewModelScope.launch{
+        repositorio.insertarTarea(tareaC)
+
+    }
+
+}
